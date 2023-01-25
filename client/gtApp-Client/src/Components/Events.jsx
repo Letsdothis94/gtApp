441a645ec9f08ef2bdab1a98c279086d39e0d785
Ryan Andujar <randujar711@gmail.com>
2023-01-25 09:17:05 -0500

    WIP: 874b242 Merge pull request #4 from Letsdothis94/ryan

--- client/gtApp-Client/src/Components/Events.jsx
+++ client/gtApp-Client/src/Components/Events.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react"
 import { Link } from "react-router-dom" 
 import './Css/Home.css'
+import Filter from './Filter'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 function Events({selEvent, setSelEvent, eventData, setEventData}){
     const [searchEvent, setSearchEvent] = useState("")
 
@@ -10,28 +19,51 @@ function Events({selEvent, setSelEvent, eventData, setEventData}){
         setSelEvent([...selEvent, e])
         console.log('event added')
     }
+    const handleFilterName = (name) => {
+    const filteredData = allData.filter((item) => {
+      const fullName = `${item.title}`;
+      if (fullName.toLowerCase().includes(name.toLowerCase())) {
+        return item;
+      }
+    });
 
-
-    const handleEventSearch = (e) => setSearchEvent(e.target.value)
+    setEventData(filteredData);
+  };
+    // const handleEventSearch = (e) => setSearchEvent(e.target.value)
     return(
-        <div className="event-cont">
-            <h1>event page</h1>
-            <input type="text" onChange={handleEventSearch} />
+        <div className="event-cont" style={{padding: '5%', background: 'rgba(113, 108, 108, 0.46)', borderRadius: '2%', display: 'flex', flexDirection: 'column'}}>
+            <Filter onNameFilter={handleFilterName}/>
             {
-                eventData.filter(event => event.title.includes(searchEvent)).map((event, i)=> {
+                eventData.map((event, i)=> {
                     return(
-                        <div key={event.id}>
-                            <h2>event data</h2>
-                            <ul>
-                                <li key={event.id}>{event.title}</li>
-                                <li>{event.date}</li>
-                            </ul>
-                            <Link to={'/selEvent'}><button onClick={()=> handleEvent(event)}>Interested
-                            ?</button></Link>
+                        <div className='card-cont' key={event.id}>
+                            <Card style={{background:'none', borderRadius: '15px', color: '#fff', }} sx={{ mmaxWidth: 200 }}>
+                                <CardContent>
+                                    <Typography sx={{ fontSize: 14 }} gutterBottom>
+                                    {event.location}
+                                    </Typography>
+                                    <hr></hr>
+                                    <Typography variant="h5" component="div">
+                                    Event: {event.title}
+                                    </Typography>
+                                    <Typography sx={{ mb: 1.5 }}>
+                                    {event.about}
+                                    </Typography>
+                                    <Typography variant="body2">
+                                    Date: {event.date}
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions >
+                                    <Link to={'/selEvent'}><Button style={{color: '#fff', textDecoration: 'none'}} size="large" onClick={()=> handleEvent(event)}>Learn More</Button></Link>
+                                </CardActions>
+                            </Card>
                         </div>
                     )
                 })
             }
+            
         </div>
     )
 }
