06b15a2b3c010ff1c22106ec728ee44e913279fb
Ryan Andujar <randujar711@gmail.com>
2023-01-26 13:44:03 -0500

    test

--- client/gtApp-Client/src/Components/Header.jsx
+++ client/gtApp-Client/src/Components/Header.jsx
@@ -2,10 +2,10 @@ import BasicMenu from './Menu'
 
 function Header({confirmEvent, loginData, setLoginData}){
     return(
-        <div style={{display:'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1%', backgroundColor:'black'}}>
+        <div style={{display:'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1%', backgroundColor:'#1A1A1D',color: '#FF5A5F'}}>
             <BasicMenu confirmEvent={confirmEvent}/>
             <h1>Title</h1>
-            <h1>Logo</h1>
+            <h1 style={{color: '#E77170'}}>Logo</h1>
         </div>
     )
 }
