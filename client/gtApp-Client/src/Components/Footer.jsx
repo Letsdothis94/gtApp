c6755ecf495000b9aa92643340fae80b3a2ab0cf
Ryan Andujar <randujar711@gmail.com>
2023-01-27 13:04:28 -0500

    old filter added

--- client/gtApp-Client/src/Components/Footer.jsx
+++ client/gtApp-Client/src/Components/Footer.jsx
@@ -3,8 +3,8 @@ import CopyrightIcon from '@mui/icons-material/Copyright';
 
 function Footer() {
   return (
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1%', backgroundImage: "linear-gradient( #1A1A1D, #000)" }}>
-        <h4 style={{display: 'flex', alignItems: 'center', gap: '5px' }}>Copyright <CopyrightIcon/> GT APP</h4>
+      <div className='footer-cont' style={{ display: 'flex', justifyContent: 'end', alignItems: 'center', padding: '1%', backgroundImage: "linear-gradient( #1A1A1D, #000)" }}>
+        <h4 style={{display: 'flex', alignItems: 'center', gap: '5px'}}>Copyright <CopyrightIcon/> GT APP</h4>
     </div>
   )
 }
